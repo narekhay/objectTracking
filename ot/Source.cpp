df778f92aaf524dab468af3210bc83624abfb6fc
Aghasy Poghosyan <aghasy@picsart.com>
2015-05-05 03:16:47 +0400

    trying clastering

--- ot/Source.cpp
+++ ot/Source.cpp
@@ -3,6 +3,7 @@
 #include "Utils.h"
 
 #include <stdio.h>
+#include <iostream>
 
 using namespace cv;
 using namespace std;
@@ -63,17 +64,18 @@ int main(int /*argc*/, const char** /*argv*/)
 			{
 				Mat out_frame_k;
 				warpAffine(frame, out_frame_k, stabiliaztion_mat, frame.size(), INTER_LINEAR | WARP_INVERSE_MAP);
-				
+
 				Mat frame_avg, last_frame_avg;
 
 				Mat ker1, ker2;
 				int k_size = 3;
+
+
 				auto sigma = 0.3*((k_size - 1)*0.5 - 1) + 0.8;
 				int kernel_size = 31;
 				double sig = 1, th = 0, lm = 1.0, gm = 0.02, ps = 0;
 				ker1 = getGaussianKernel(k_size, sigma, CV_32F);
 				auto ker5 = getGaborKernel(Size(kernel_size, kernel_size), gm, th, lm, ps);
-				imshow("Gabor", ker5);
 
 				ker2 = ker1;
 
@@ -84,7 +86,7 @@ int main(int /*argc*/, const char** /*argv*/)
 				auto s2 = last_frame_avg.data;
 				test_frame = last_frame.clone();
 				auto oData = test_frame.data;
-				
+
 				auto w = out_frame.size().width;
 				auto h = out_frame.size().height;
 				for (size_t i = 0; i < h; i++)
@@ -97,11 +99,42 @@ int main(int /*argc*/, const char** /*argv*/)
 					}
 				}
 
+				Mat ioarr, cent_mat, k_mat;
+				test_frame.convertTo(k_mat, CV_32F);
 
+				Mat tk = k_mat.reshape(1,k_mat.size().area());
+
+				kmeans(k_mat, 8, ioarr, TermCriteria(TermCriteria::Type::MAX_ITER, 10000, 0.1), 9, KMEANS_PP_CENTERS, cent_mat);
 				//test_frame = out_frame_k - last_frame;
 				//out_frame = last_frame - out_frame_k;
+		
+				// i think there is a better way to do this mayebe some Mat::reshape?
 
 
+
+				//Mat clustered = Mat(k_mat.rows, k_mat.cols, CV_32F);
+				//for (int i = 0; i < k_mat.cols*k_mat.rows; i++) {
+				//	auto indx = ioarr.at<int>(i) ;
+				//	clustered.at<float>(i / k_mat.cols, i%k_mat.cols) = 0; indx == 3 ? 0 : 255;
+				//}
+
+				//clustered.convertTo(clustered, CV_8U);
+				//imshow("clustered", clustered);
+
+
+				Mat centres_mat = Mat::zeros(k_mat.rows, k_mat.cols, CV_8U);
+
+				for (size_t i = 0; i < cent_mat.cols; i++)
+				{
+					for (size_t j = 0; j < cent_mat.rows; j++)
+					{
+						centres_mat.at<uchar>((int)cent_mat.at<float>(j, i), i) = 255;
+					}
+				}
+
+				Mat comp[] = { frame_avg, centres_mat, centres_mat };
+
+				merge(comp, 3, test_frame);
 			}
 			
 		}
