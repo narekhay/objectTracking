4f8a984532d0501937a4a422d4fb9b72ca5decd5
Aghasy Poghosyan <aghasy@picsart.com>
2015-05-04 23:19:18 +0400

    some

--- ot/Source.cpp
+++ ot/Source.cpp
@@ -27,7 +27,7 @@ int main(int /*argc*/, const char** /*argv*/)
 	//namedWindow(outWinName, 1);
 	namedWindow(testWinName, 1);
 	
-	Mat frame, last_frame, test_frame, out_frame, m_frame, m_last_frame, d_last_frame;
+	Mat frame, last_frame, test_frame, out_frame;
 	auto fps = video_capture.get(CAP_PROP_FPS);
 
 	for (int frame_index = 0; ; frame_index++)
@@ -45,112 +45,12 @@ int main(int /*argc*/, const char** /*argv*/)
 			continue;
 		}
 		cvtColor(frame, frame, CV_BGR2GRAY);
-		Mat dx, dy;
-		Sobel(frame, dx, CV_8U, 1, 0);
-		Sobel(frame, dy, CV_8U, 0, 1);
-		convertScaleAbs(dx, dx);
-		convertScaleAbs(dy, dy);
-		//addWeighted(dx, 0.5, dy, 0.5, 0, frame);
-		Mat g_hist;
-		int hist_size[] = { 256 };
-		int hist_chaneles[] = { 0 };
-		float hist_range[] = { 0, 256 };
-		const float * hist_range_t[]= {hist_range};
-		calcHist(&frame, 1, hist_chaneles, Mat(), g_hist, 1, hist_size, hist_range_t);
+
 		
-		test_frame = Mat(g_hist.size().height, 100, g_hist.type());
-		double max_histogram_value, min_histogram_value;
-		minMaxIdx(g_hist, &min_histogram_value, &max_histogram_value);
-		
-
-		float previousHistogramValue = g_hist.at<float>(0);;
-		bool histogram_was_growing = true;
-		vector<int> breaking_points_vector;
-		breaking_points_vector.push_back(0);
-
-		/*for (size_t i = 1; i < 255; i++)
-		{
-			auto current_histogram_value = g_hist.at<float>(i);
-			if (abs(current_histogram_value - previousHistogramValue) > max_histogram_value / 10)
-			{
-				bool isGrowing = current_histogram_value - previousHistogramValue > 0;
-				if (!histogram_was_growing && isGrowing)
-				{
-					breaking_points_vector.push_back(i);
-				}
-				previousHistogramValue = current_histogram_value;
-				histogram_was_growing = isGrowing;
-			}
-		}*/
-		breaking_points_vector.push_back(50);
-		breaking_points_vector.push_back(100);
-
-		breaking_points_vector.push_back(150);
-		breaking_points_vector.push_back(200);
-		
-		breaking_points_vector.push_back(255);
-
-		double maximalPixelValue;
-		double minimalPixelValue;
-		minMaxIdx(frame, &minimalPixelValue, &maximalPixelValue);
-		frame.copyTo(m_frame);
-		for (size_t k = 0; k < m_frame.total(); k++)
-		{
-			for (auto i = breaking_points_vector.begin(); i < breaking_points_vector.end(); ++i)
-			{
-				if (m_frame.at<unsigned char>(k) > maximalPixelValue - 18)
-				{
-					m_frame.at<unsigned char>(k) = 255;
-				}
-					else 
-				if (*i >= m_frame.at<unsigned char>(k))
-				{
-					if (i <= breaking_points_vector.begin() + 1)
-					{
-						m_frame.at<unsigned char>(k) = 0;
-					}
-					else if (i == breaking_points_vector.end() - 1)
-					{
-						m_frame.at<unsigned char>(k) = 255;
-					}
-					else
-					{
-						m_frame.at<unsigned char>(k) = (*i + (*i -1 ))/2;
-					}
-					break;
-				}
-			}
-		}
-		/*
-		m_frame.forEach<unsigned char>([=](unsigned char &vale, const int * pos)
-		{
-			for (auto i = breaking_points_vector.begin(); i < breaking_points_vector.end(); ++i)
-			{
-				if (*i >= vale)
-				{
-					if (i <= breaking_points_vector.begin() + 1)
-					{
-						vale = 0;
-					}
-					else if (i == breaking_points_vector.end() - 1)
-					{
-						vale = 255;
-					}
-					else
-					{
-						vale = (*i + *(i - 1)) / 2;
-					}
-					break;
-				}
-			}
-		});
-		*/
-		
-
 		if (last_frame.empty())
 		{
 			//fisrt frame
-			d_last_frame = frame;
+			
 		} 
 		else
 		{
@@ -161,12 +61,10 @@ int main(int /*argc*/, const char** /*argv*/)
 			} 
 			else
 			{
-				Mat out_frame_k, out_m_frame_k;
-				warpAffine(m_frame, out_m_frame_k, stabiliaztion_mat, frame.size(), INTER_LINEAR | WARP_INVERSE_MAP);
-				out_frame = m_last_frame - out_m_frame_k;
+				Mat out_frame_k;
 				warpAffine(frame, out_frame_k, stabiliaztion_mat, frame.size(), INTER_LINEAR | WARP_INVERSE_MAP);
-				test_frame = d_last_frame - out_frame_k;
-				d_last_frame = test_frame;
+				out_frame = out_frame_k;
+				test_frame = last_frame;
 			}
 			
 		}
@@ -174,7 +72,6 @@ int main(int /*argc*/, const char** /*argv*/)
 		auto freq = getTickFrequency();
 		auto exec_time = (toc - tic) / freq * 1000;
 		last_frame = frame;
-		m_last_frame = m_frame;
 
 		printf("Processing: %d time: %f ms +time:%f \n", frame_index, exec_time, 1000 / fps - exec_time);
 
@@ -187,7 +84,7 @@ int main(int /*argc*/, const char** /*argv*/)
 		{
 			imshow(outWinName, out_frame);
 		}
-		waitKey(0/*MAX(10,1000/fps - exec_time)*/);
+		waitKey(1000/*MAX(10,1000/fps - exec_time)*/);
 	}
 	
 
