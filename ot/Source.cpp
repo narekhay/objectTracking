79ec693c1d68b9f5d312c96e7cb0a8c84d4a1c57
Aghasy Poghosyan <aghasy@picsart.com>
2015-05-05 01:35:23 +0400

    gaus detection

--- ot/Source.cpp
+++ ot/Source.cpp
@@ -63,8 +63,45 @@ int main(int /*argc*/, const char** /*argv*/)
 			{
 				Mat out_frame_k;
 				warpAffine(frame, out_frame_k, stabiliaztion_mat, frame.size(), INTER_LINEAR | WARP_INVERSE_MAP);
-				out_frame = out_frame_k;
-				test_frame = last_frame;
+				
+				Mat frame_avg, last_frame_avg;
+
+				Mat ker1, ker2;
+				int k_size = 3;
+				auto sigma = 0.3*((k_size - 1)*0.5 - 1) + 0.8;
+				int kernel_size = 31;
+				double sig = 1, th = 0, lm = 1.0, gm = 0.02, ps = 0;
+				ker1 = getGaussianKernel(k_size, sigma, CV_32F);
+				auto ker5 = getGaborKernel(Size(kernel_size, kernel_size), gm, th, lm, ps);
+				imshow("Gabor", ker5);
+
+				ker2 = ker1;
+
+				filter2D(out_frame_k, frame_avg, -1, ker1);
+				filter2D(last_frame, last_frame_avg, -1, ker1);
+				out_frame = frame_avg;
+				auto s1 = frame_avg.data;
+				auto s2 = last_frame_avg.data;
+				test_frame = last_frame.clone();
+				auto oData = test_frame.data;
+				
+				auto w = out_frame.size().width;
+				auto h = out_frame.size().height;
+				for (size_t i = 0; i < h; i++)
+				{
+					for (size_t j = 0; j < w; j++)
+					{
+						auto p1 = *((s1 + j) + w*i);
+						auto p2 = *((s2 + j) + w*i);
+						*((oData + j) + w*i) = abs(p1 - p2);
+					}
+				}
+
+
+				//test_frame = out_frame_k - last_frame;
+				//out_frame = last_frame - out_frame_k;
+
+
 			}
 			
 		}
